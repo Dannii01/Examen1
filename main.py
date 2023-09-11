10798bea3488c40fff75fd49e1e85e085bad0733
dannii01 <danijavier4@gmail.com>
2023-09-11 09:19:50 -0500

    Extraccion de datos

---
+++ main.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+consulta = "zapatos"
+driver = webdriver.Chrome()
+driver.get("https://www.nike.com/us/es/")
+search_box = driver.find_element(by=By.CSS_SELECTOR, value="#VisualSearchInput")
+
+search_box.send_keys(consulta)
+
+search_button = driver.find_element(by=By.CSS_SELECTOR, value="#gen-nav-commerce-header-v2 > div.pre-l-header-container > header > div > div.pre-l-wrapper.mauto-sm.d-sm-flx > div.pre-l-nav-box.flx-gro-sm-1 > div > div > div.d-sm-flx.flx-jc-lg-fe.u-position-rel > div > div > button.pre-search-btn.ripple")
+search_button.click()
+
+shoes = driver.find_elements(By.CSS_SELECTOR, "#skip-to-products >div")
+
+for card in shoes:
+   try:
+        title = card.find_element(By.CSS_SELECTOR, "div > figure > a.product-card__link-overlay").text
+        subtitle = card.find_element(By.CSS_SELECTOR, "div > figure > div > div.product_msg_info > div.product-card__titles > div.product-card__subtitle").text
+        color = card.find_element(By.CSS_SELECTOR, "div > figure > div > div.product-card__count-wrapper.show--all.false > div > button > div").text
+        price = card.find_element(By.CSS_SELECTOR, "div > figure > div > div.product-card__animation_wrapper > div").text
+        print(title)
+        print(subtitle)
+        print(color)
+        print(f"{price}")
+        shoes_actual = {
+             "consulta": consulta,
+             "resultado": {
+                  "title": title,
+                  "subtitle": subtitle,
+                  "color": color,
+                  "price": price,
+             }
+        }
+
+        db = mongo_client.get_database('db_shoes')
+        collection = db.get_collection(f'buscozapatos')
+
+        collection.insert_one(shoes_actual)
+
+        print("++++++++++++++++++++++++++++++++")
+   except Exception as e:
+        print(e)
+        print("++++++++++++++++++++++++++++++++")
+
+
+driver.close()
